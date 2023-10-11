6de64dd870184cf453e76b059d49c65936b5dc94
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-11 23:25:43 +0000

    Simple Shell Project

---
+++ strcheck.c
@@ -0,0 +1,25 @@
+#include "simpleshell.h"
+
+/**
+ * search_str - checks if a character parameter is inside a string
+ * @str: string parameter
+ * @c: character being checked
+ * Return: 1 (success) 0 (otherwise)
+ */
+int _check(char *str, char c)
+{
+        int count = 0;
+
+        while (str[count] != '\0')
+        {
+                if (str[count] == c)
+                {
+                        break;
+                }
+                count++;
+        }
+        if (str[count] == c)
+                return (1);
+        else
+                return (0);
+}
