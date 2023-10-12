46c8476807ce3ac002c3ed8352614b592bb2d5d7
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-12 00:47:43 +0000

    Simple Shell Project

---
+++ tokenize.c
@@ -0,0 +1,31 @@
+#include "simpleshell.h"
+
+/**
+ * tokenize - function that splits a command line into individual tokens
+ * @command: command paramter
+ * @tokens: tokens paramter
+ * @max_tokens: max number of tokens
+ * Return: token_counts (int value)
+ */
+int tokenize(const char *command, char *tokens[], int max_tokens)
+{
+	char *token;
+	char *delim[] = " ,()";
+	int token_count = 0;
+	
+	token = _strtok(command, delim);
+	while (token != NULL)
+	{
+		if (token_count < max_tokens)
+		{
+			tokens[token_count] = token;
+			token_count++;
+			token = _strtok(NULL, delim);
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (token_count);
+}
