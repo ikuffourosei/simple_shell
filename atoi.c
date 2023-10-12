b57e5548d504872c8b09cedfd0ff230ac2e931bb
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-12 19:23:17 +0000

    Simple Shell Project

---
+++ atoi.c
@@ -0,0 +1,45 @@
+#include "simpleshell.h"
+
+/**
+ * _atoi - a function that converts a string to an integer
+ * @s: string to be converted
+ *Return: n (success) 0 (otherwise)
+ */
+int _atoi(char *s)
+{
+	int flag, i, n, d, len, digit;
+
+	flag = 0;
+	i = 0;
+	n = 0;
+	d = 0;
+	len = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && flag == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			flag = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			flag = 0;
+		}
+		i++;
+	}
+
+	if (flag == 0)
+		return (0);
+
+	return (n);
+}
