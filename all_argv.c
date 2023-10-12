6d77da1d42341088397493c414c278bec5a0c5ac
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-12 22:09:37 +0000

    Simple Shell Project

--- all_argv.c
+++ all_argv.c
@@ -39,7 +39,7 @@ void _readargv(char *argv[], char *_env[])
 					ptr[i] = parse[i];
 				ptr[i] = '\n';
 				ptr[i + 1] = '\0';
-				functions(ptr, loop, argv, &env_copy, &j, line);
+				shfunc(ptr, loop, argv, &env_copy, &j, line);
 				parse = _strtok2(NULL, ";\n");
 			}
 		}
@@ -47,4 +47,50 @@ void _readargv(char *argv[], char *_env[])
 	}
 }
 
+/**
+ * _read - function for shell execution 
+ * without command-line arguments.
+ * @argv: arguments vector
+ * @_env: environment variables
+ */
+void _read(char *argv[], char *_env[])
+{
+	char *ptr, *line, *parse, **env_copy;
+	int i, j, sc, *loop_track;
+	static int loop;
+	
+	ptr1 = NULL;
+	line = NULL;
+	parse = NULL ;
+	env_copy = NULL;
+	loop = 0;
+	j = 0;
+	loop_track = &loop;
 
+	while (1)
+        {
+		if (loop == 0)
+		{
+			env_copy = create_env(_env);
+			for (j = 0; env_copy[j] != NULL; j++)
+				;
+		}
+		line = _getline(loop_track, env_copy, j);
+		sem = semicolon(line, loop, argv);
+		if (!(sc == 1))
+		{
+			parse = _strtok2(line, ";\n");
+			while (parse)
+			{
+				ptr = _calloc(strlength(p) + 2, sizeof(char));
+				for (i = 0; parse[i] != '\0'; i++)
+					ptr[i] = parse[i];
+				ptr[i] = '\n';
+				ptr[i + 1] = '\0';
+				shfunc(ptr, loop, argv, &env_copy, &j, line);
+				p = _strtok2(NULL, ";\n");
+			}
+		}
+		free(line);
+	}
+}
