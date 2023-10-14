78df5ef91f485691947160973191ecf9dcea0ee3
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-14 04:11:10 +0000

    Simple Shell Project

---
+++ printnum.c
@@ -0,0 +1,31 @@
+#include "simpleshell.h"
+
+/**
+ * print_n - function that prints all naturalumbers 
+ * @number: number to print
+ */ 
+void print_n(int number)
+{
+	unsigned int i, n, num1, num2, num3, count;
+	char c;
+
+	count = 1;
+	num3 = 1;
+	num2 = number;
+	n = num2;
+
+	while (n > 9)
+	{
+		n /= 10;
+		num3 *= 10;
+		count++;
+	}
+	for (i = 1; i <= count; i++)
+	{
+		num1 = num2 / num3;
+		num2 %= num3;
+		num3 /= 10;
+		c = (num1 + '0');
+		puterror(c);
+	}
+}
