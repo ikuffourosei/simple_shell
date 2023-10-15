b0710f49186aed3e110e926cc00023a4dc71db53
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-15 01:30:48 +0000

    Simple Shell Proejct

---
+++ simpleshell.c
@@ -0,0 +1,27 @@
+#include "simpleshell.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments in the input
+ * @env: environment variables
+ * Return: 0 on Success
+ */
+int main(int argc, char *argv[], char *env[])
+{
+
+	if (argc == 1)
+	{
+		_read(argv, env);
+	}
+	else if (argc == 2)
+	{
+		_readargv(argv, env);
+	}
+	else
+	{
+		_print("NO ADMITTED AMOUNT OF ARGUMENTS");
+		_putchar('\n');
+	}
+	return (0);
+}
