9bf691dcaadeed6af2f3b4855f93c39ff7c56781
Osei Kuffour Emmanuel <138737059+ikuffourosei@users.noreply.github.com>
2023-10-18 22:43:29 +0000

    Update setenv_check.c

--- setenv_check.c
+++ setenv_check.c
@@ -7,22 +7,26 @@
  */
 int _issetenv(char *command)
 {
-	char *tokens[4];
+	char *tokens[3];
 	int num_tokens;
 
 	if (_strncmp(command, "setenv", 6) == 0)
 	{
-		num_tokens = tokenize(command, tokens, 4);
+		num_tokens = tokenize(command, tokens, 3);
 
 		if (num_tokens >= 3)
-	{
-		return (_setenv(tokens[1], tokens[2], atoi(tokens[3])));
-	}
-	else
-	{
-		printerror("Usage: setenv NAME VALUE\n");
-		return (-1);
-	}
+		{
+			return (_setenv(tokens[1], tokens[2], 1);
+		}
+		else if (num_tokens == 2)
+		{
+			return _setenv(tokens[1], "", 1);
+		}
+		else
+		{
+			printerror("Usage: setenv NAME VALUE\n");
+			return (-1);
+		}
 	}
 	else
 	{
