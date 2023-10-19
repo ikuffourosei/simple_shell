7ca238cb6ffa4b75428a415cc8e75e420e6f2588
Franciskojo <franciskojoamekeh@gmail.com>
2023-10-18 23:18:04 -0700

    update

--- setenv_check.c
+++ setenv_check.c
@@ -7,30 +7,30 @@
  */
 int _issetenv(char *command)
 {
-    char *tokens[3];
-    int num_tokens;
+	char *tokens[3];
+	int num_tokens;
 
-    if (_strncmp(command, "setenv", 6) == 0)
-    {
-        num_tokens = tokenize(command, tokens, 3);
+	if (_strncmp(command, "setenv", 6) == 0)
+	{
+		num_tokens = tokenize(command, tokens, 3);
 
-        if (num_tokens == 3)
-        {
-            return _setenv(tokens[1], tokens[2], 1);
-        }
-        else if (num_tokens == 2)
-        {
-            return _setenv(tokens[1], "", 1);
-        }
-        else
-        {
-            printerror("Usage: setenv NAME VALUE");
-	    puterror('\n');
-            return (-1);
-        }
-    }
-    else
-    {
-         return (-1);
-    }
+		if (num_tokens == 3)
+		{
+			return (_setenv(tokens[1], tokens[2], 1));
+		}
+		else if (num_tokens == 2)
+		{
+			return (_setenv(tokens[1], "", 1));
+		}
+		else
+		{
+			printerror("Usage: setenv NAME VALUE");
+			puterror('\n');
+			return (-1);
+		}
+	}
+	else
+	{
+		return (-1);
+	}
 }
